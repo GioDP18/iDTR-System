d0b67b7c7e05c24ca48495e9dfbe358d8fc2c579
GioDP18 <giolagariza@gmail.com>
2024-01-20 17:27:08 +0800

    Added time-out functionality for AM

--- backend/API.php
+++ backend/API.php
@@ -25,29 +25,31 @@ class Master extends DBConnection{
 	}
 
 
-    function time_in_am() {
+    function time_in_am(){
 		extract($_POST);
 		$timeAbbreviation = date('A');
-		if ($timeAbbreviation === 'AM') {
+		if($timeAbbreviation === 'AM'){
 			$checkQuery = $this->conn->query("SELECT * FROM `daily_time_records` WHERE intern_id = '$intern_id' AND date = '$date'");
-			
-			if ($checkQuery->num_rows > 0) {
+			if($checkQuery->num_rows > 0){
 				$response['success'] = false;
 				$response['message'] = "You have already time-in on this morning.";
-			} else {
+			}
+			else{
 				$stmt = $this->conn->prepare("INSERT INTO `daily_time_records` (intern_id, arrival_am, date) VALUES (?, ?, ?)");
 				$stmt->bind_param("sss", $intern_id, $time, $date);
 				$result = $stmt->execute();
 	
-				if ($result) {
+				if($result){
 					$response['success'] = true;
-				} else {
+				}
+				else{
 					$response['success'] = false;
 					$response['message'] = "There was an error logging your time-in. Please try again later.";
 				}
 				$stmt->close();
 			}
-		} else {
+		}
+		else{
 			$response['success'] = false;
 			$response['message'] = "It's already PM.";
 		}
@@ -57,7 +59,49 @@ class Master extends DBConnection{
 	}
 	
 
-	
+	function time_out_am(){
+		extract($_POST);
+		$hour = date('H');
+		if($hour <= '12'){
+			$checkQuery = $this->conn->query("SELECT * FROM `daily_time_records` WHERE intern_id = '$intern_id' AND date = '$date'");
+			if($checkQuery->num_rows < 0){
+				$response['success'] = false;
+				$response['message'] = "Opss! You have not time-in on this morning.";
+			}
+			elseif($checkQuery->num_rows === 1){
+				$checkResult = $checkQuery->fetch_assoc();
+				if(!$checkResult['departure_am']){
+					$stmt = $this->conn->prepare("UPDATE `daily_time_records` SET departure_am =? WHERE intern_id =? AND date =?");
+					$stmt->bind_param("sss", $time, $intern_id, $date);
+					$result = $stmt->execute();
+					if($result){
+						$response['success'] = true;
+					}
+					else{
+						$response['success'] = false;
+						$response['message'] = "There was an error logging your time-out. Please try again later.";
+					}
+					$stmt->close();
+				}
+				else{
+					$response['success'] = false;
+                    $response['message'] = "You have already time-out on this noon.";
+				}
+				
+			}
+			else{
+				$response['success'] = false;
+                $response['message'] = "Your log record for this day has exceeded its limit.";
+			}
+		}
+		else{
+			$response['success'] = false;
+			$response['message'] = "It's already PM.";
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+		exit;
+	}
 	
 }
 
